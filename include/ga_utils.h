b4bf29e6cb658c72d57068d78cf2ec09bf731aa4
Nick Tsiogkas <nt95@hw.ac.uk>
2016-04-03 21:04:19 +0100

    Moved the function of the population sample generation as it is used in more than one GA method.

--- include/ga_utils.h
+++ include/ga_utils.h
@@ -6,7 +6,10 @@
 #define LWGA_GA_UTILS_H
 
 #include "ga_types.h"
+#include <random>
 
 double find_distance(Point2D a, Point2D b);
+
+std::vector<size_t> get_population_sample(size_t pop_size, int samples, std::mt19937 &g);
 
 #endif //LWGA_GA_UTILS_H
--- src/ga_utils.cpp
+++ src/ga_utils.cpp
@@ -8,3 +8,21 @@
 double find_distance(Point2D a, Point2D b) {
   return sqrt(pow(a.first - b.first, 2) + pow(a.second - b.second, 2));
 }
+
+std::vector<size_t> get_population_sample(size_t pop_size, int samples, std::mt19937 &g) {
+
+  std::vector<size_t> indices(pop_size);
+  std::iota(indices.begin(), indices.end(), 0);
+  size_t max = indices.size() - 1;
+
+  std::vector<size_t> result;
+
+  for (int i = 0; i < samples; ++i) {
+    std::uniform_int_distribution<> d(0, max);
+    size_t index = d(g);
+    std::swap(indices[index], indices[max]);
+    result.push_back(indices[max]);
+    max--;
+  }
+  return result;
+}
