f5ac06759848cec355862355cdc83cb9f018e584
Niko Tsiogkas <lou.nick@gmail.com>
2019-10-22 17:33:01 +0200

    Add string names

--- include/libtsp_reader.h
+++ include/libtsp_reader.h
@@ -76,9 +76,12 @@ class LIBTSPReader {
   Vector<double_t> GetProbabilityVector();
   Vector<double_t> GetRewardsVector();
   double_t GetCapacity();
+  void ParseFile();
 
  private:
-  void ParseFile();
+  void HandleHeaderEntry(const std::string &line);
+  void HandleDataEntry(const std::string &line);
+
   std::string file_name_;
   std::string problem_name_;
   ProblemType problem_type_;
@@ -92,6 +95,56 @@ class LIBTSPReader {
   Vector<double_t> probabilities_;
   Vector<double_t> rewards_;
   double_t capacity_;
+
+  const std::string kNameIdentifier = "NAME";
+  const std::string kTypeIdentifier = "TYPE";
+  const std::string kCommentIdentifier = "COMMENT";
+  const std::string kDimensionIdentifier = "DIMENSION";
+  const std::string kCapacityIdentifier = "CAPACITY";
+  const std::string kEdgeWeightTypeIdentifier = "EDGE_WEIGHT_TYPE";
+  const std::string kEdgeWeightFormatIdentifier = "EDGE_WEIGHT_FORMAT";
+  const std::string kEdgeDataFormatIdentifier = "EDGE_DATA_FORMAT";
+  const std::string kNodeCoordTypeIdentifier = "NODE_COORD_TYPE";
+  const std::string kDisplayDataTypeIdentifier = "DISPLAY_DATA_TYPE";
+  const std::string kEOFIdentifier = "EOF";
+
+  const std::string kTSPIdentifier = "TSP";
+  const std::string kATSPIdentifier = "ATSP";
+  const std::string kSOPIdentifier = "SOP";
+  const std::string kHCPIdentifier = "HCP";
+  const std::string kCVRPIdentifier = "CVRP";
+  const std::string kTOURIdentifier = "TOUR";
+  const std::string kPOPIdentifier = "POP";
+
+  const std::string kEuc2DIdentifier = "EUC_2D";
+  const std::string kEuc3DIdentifier = "EUC_3D";
+  const std::string kMax2DIdentifier = "MAX_2D";
+  const std::string kMax3DIdentifier = "MAX_3D";
+  const std::string kCeil2DIdentifier = "CEIL_2D";
+  const std::string kExplicitIdentifier = "EXPLICIT";
+  const std::string kGeopIdentifier = "GEO";
+  const std::string kAttIdentifier = "ATT";
+  const std::string kXRay1Identifier = "XRAY1";
+  const std::string kXRay2Identifier = "XRAY2";
+  const std::string kSpecialIdentifier = "SPECIAL";
+
+  const std::string kFunctionIdentifier = "FUNCTION";
+  const std::string kFullMatrixIdentifier = "FULL_MATRIX";
+  const std::string kUpperRowIdentifier = "UPPER_ROW";
+  const std::string kLowerRowIdentifier = "LOWER_ROW";
+  const std::string kUpperDiagRowIdentifier = "UPPER_DIAG_ROW";
+  const std::string kLowerDiagRowIdentifier = "LOWER_DIAG_ROW";
+  const std::string kUpperColIdentifier = "UPPER_COL";
+  const std::string kLowerColIdentifier = "LOWER_COL";
+  const std::string kUpperDiagColIdentifier = "UPPER_DIAG_COL";
+  const std::string kLowerDiagColIdentifier = "LOWER_DIAG_COL";
+
+  const std::string kTwoDIdentifier = "TWO_D";
+  const std::string kThreeDIdentifier = "THREE_D";
+
+  const std::string kCoordDisplayIdentifier = "COORD_DISPLAY";
+  const std::string kTwoDDisplayIdentifier = "TWOD_DISPLAY";
+  const std::string kNoDisplayIdentifier = "NO_DISPLAY";
 };
 }  // namespace libtsp_reader
 #endif  // LWGA_LIBTSP_READER_H_
--- src/libtsp_reader.cpp
+++ src/libtsp_reader.cpp
@@ -1,5 +1,7 @@
 #include "libtsp_reader.h"
 
+#include <sstream>
+
 namespace libtsp_reader {
 
 LIBTSPReader::LIBTSPReader(std::string file_name) { file_name_ = file_name; }
@@ -23,6 +25,22 @@ double_t LIBTSPReader::GetCapacity() { return capacity_; }
 void LIBTSPReader::ParseFile() {
   std::string line;
   while (std::getline(file_stream_, line)) {
+    if (file_section == FileSection::HEADER) {
+      HandleHeaderEntry(line);
+    } else {
+      HandleDataEntry(line);
+    }
   }
 }
+void LibTSPReader::HandleHeaderEntry(const std::string &line) {
+  std::istringstream iss(line);
+  std::string key;
+  iss >> key;
+
+  // Remove the trailing colon if in the same word
+  if (key.back() == ':') {
+    key.pop_back();
+  }
+}
+void LibTSPReader::HandleDataEntry(const std::string &line) {}
 }  // namespace libtsp_reader
