cbaf73e7911b0b3d582eed8332efc2b84e3843d1
Niko Tsiogkas <lou.nick@gmail.com>
2020-01-26 20:29:14 +0100

    Add some libtsp tests

--- test/libtsp_reader_test.cpp
+++ test/libtsp_reader_test.cpp
@@ -1,5 +1,46 @@
+#include <iostream>
+#include <fstream>
+
 #include "gtest/gtest.h"
 #include "libtsp_reader.h"
+
+bool WriteToFile(const std::string &filename, const std::string &content) {
+  std::ofstream file(filename);
+
+  if (!file.is_open()) {
+    std::cerr << "Could not open output file" << std::endl;
+    return false;
+  }
+
+  file << content;
+  file.close();
+  return true;
+}
+
+TEST(LibTSPTest, nonExistingFileTest) {
+  std::string filename = "/tmp/bad_input.txt";
+  libtsp_reader::LIBTSPReader reader(filename);
+  EXPECT_EQ(reader.Initialise(), false);
+}
+
+TEST(LibTSPTest, existingFileTest) {
+  std::string filename = "/tmp/input.txt";
+
+  libtsp_reader::LIBTSPReader reader(filename);
+
+  EXPECT_EQ(WriteToFile(filename, ""), true);
+  EXPECT_EQ(reader.Initialise(), true);
+}
+
+TEST(LibTSPTest, parseEmptyFile) {
+  std::string filename = "/tmp/input.txt";
+
+  libtsp_reader::LIBTSPReader reader(filename);
+
+  EXPECT_EQ(WriteToFile(filename, ""), true);
+  EXPECT_EQ(reader.Initialise(), true);
+  EXPECT_EQ(reader.ParseFile(), false);
+}
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
