a3117184e97ff8ea068dee26b2e72b3a12c5b862
Niko Tsiogkas <lou.nick@gmail.com>
2020-08-02 16:47:46 +0200

    Add reading file

--- src/pop_ga_main.cpp
+++ src/pop_ga_main.cpp
@@ -1,10 +1,12 @@
+#include "libtsp_reader.h"
+#include <iostream>
 #include "pop_ga.h"
 
 int main(int argc, char *argv[]) {
   pop_ga::Properties properties;
   properties.generation_method = pop_ga::GenerationMethod::GRASP;
   properties.start_id = 0;
-  properties.end_id = 10;
+  properties.end_id = 0;
   properties.maximum_cost = 10;
   properties.cost_per_time_unit = 1;
   properties.grasp_greediness = 0.5;
@@ -19,5 +21,17 @@ int main(int argc, char *argv[]) {
   properties.cx_rate = 0.3;
   properties.mutation_rate = 0.7;
   properties.num_threads = 6;
+
+  if (argc < 2) {
+    std::cout << "I need a file to read the problem from!" << std::endl;
+    return 1;
+  }
+
+  const std::string filename = std::string(argv[1]);
+
+  libtsp_reader::LIBTSPReader reader(filename); 
+  reader.Initialise();
+  reader.ParseFile();
+
   return 0;
 }
